008af085fb4b8672126686fe1a6ea54803f22b61
lwwywang <14119472+lwwywang@user.noreply.gitee.com>
2024-07-19 15:34:38 +0800

    74-搜索二维矩阵

---
+++ 2-栈和队列（Stack & Queue）/8-搜索二维矩阵/74-搜索二维矩阵.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+
+    if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        return false;
+    }
+
+    const row = matrix.length;
+    const column = matrix[0].length;
+
+    let start = 0;
+    let end = row * column -1;
+    while (start + 1 < end) {
+        const mid = Math.floor(start + (end - start) / 2);
+
+        const x = Math.floor(mid / column);
+        const y = mid % column;
+        if(target == matrix[x][y]){
+            return true;
+        }else if(target > matrix[x][y]){
+            start = mid;
+        }else{
+            end = mid;
+        }
+    }
+    if(matrix[Math.floor(start / column)][start % column] == target || matrix[Math.floor(end / column)][end % column] == target) {
+            return true;
+    }
+    return false
+    
+};  
+
+const matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
+const target = 3;
+console.log(searchMatrix(matrix, target)); // true
