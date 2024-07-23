1e21a443ed0c620061d9f5bf1413bc36b605e984
lwwywang <14119472+lwwywang@user.noreply.gitee.com>
2024-07-23 11:14:01 +0800

    环形链表

---
+++ 3-链表（List）/3-141/141-环形链表.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(head === null || head.next === null) {
+        return false;
+    }
+
+    let fast = head;
+    let slow = head;
+
+    while(fast != null || fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if(fast === slow) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+function createLinkedList(arr) {
+    let head = new ListNode(0); 
+    let current = head;
+    for (let val of arr) {
+        current.next = new ListNode(val);
+        current = current.next;
+    }
+    return head.next;
+}
+
+const head = createLinkedList([3,2,0,-4]);
+head.next.next.next.next = head.next;
+console.log(hasCycle(head)); // true
---
+++ 3-链表（List）/4-142/142-环形链表 II.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var detectCycle = function(head) {
+    if(head === null || head.next === null) {
+        return false;
+    }
+
+    let fast = head;
+    let slow = head;
+
+    while(fast != null || fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if(fast === slow) {
+            current = head;
+            while(current != slow) {
+                current = current.next;
+                slow = slow.next;
+            }
+
+            return current;
+        }
+    }
+
+    return false;
+};
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+function createLinkedList(arr) {
+    let head = new ListNode(0); 
+    let current = head;
+    for (let val of arr) {
+        current.next = new ListNode(val);
+        current = current.next;
+    }
+    return head.next;
+}
+
+const head = createLinkedList([3,2,0,-4]);
+head.next.next.next.next = head.next;
+console.log(detectCycle(head)); // 2
